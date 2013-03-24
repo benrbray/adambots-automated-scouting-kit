a1af6dc404d5b5548c5b041166a5a9b88c3006e7
Ben Bray <archon68@gmail.com>
2013-03-24 13:31:42 -0400

    Fixed up color stuff and organized.  Also created NetBeans project.

--- java/AASK/src/root/Main.java
+++ java/AASK/src/root/Main.java
@@ -1,7 +1,11 @@
 package root;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Arrays;
 import java.util.Hashtable;
 
@@ -10,7 +14,7 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import root.graphics.ColorUtils;
+import utils.ColorUtils;
 
 import Jama.Matrix;
 
@@ -31,37 +35,8 @@ public class Main {
 	}
 
 	//// INITIALIZATION -------------------------------------------------------
-	
-	public double[] colorLerp(double[] c1,double[] c2,double v)
-	{
-		return new double[] {c1[0]*(1-v) + c2[0]*v , c1[1]*(1-v) + c2[1]*v , c1[2]*(1-v) + c2[2]*v };
-	}
-	
-	public String getWarmCold(double lo,double val,double hi)
-	{
-                double md = (lo + hi)/2;
-                double hf = (hi - lo)/2;
-                double[] A = {227,137,147};
-                double[] B = {247,247,137};
-                double[] C = {152,227,167};
-                if (val < md) {
-                        double x = (val - lo) / hf;
-                        return colorToHex(colorLerp(A,B,x));
-                }
-                double x = (val - md) / hf;
-                return colorToHex(colorLerp(B,C,x));
-	}
-	
-	String colorToHex(double[] m)
-	{
-		int a = (int)m[0];
-		int b = (int)m[1];
-		int c = (int)m[2];
-		String f = "0123456789ABCDEF";
-		return "#" + f.charAt(a/16) + f.charAt(a%16) + f.charAt(b/16) + f.charAt(b%16) + f.charAt(c/16) + f.charAt(c%16);
-	}
-	
-	String truncate(double u,int a){
+
+	public String truncate(double u, int a){
 		String m = u + "";
 		if (m.indexOf(".") == -1)
 		{
@@ -83,7 +58,6 @@ public class Main {
 		Matrix totalEC = event.getTotalContributions();
 		
 		// Print Data
-		
 		double[] totalr = {totalEC.get(0,0),totalEC.get(0,0)};
 		double[] climbr = {climbEC.get(0,0),climbEC.get(0,0)};
 		double[] autonr = {autonEC.get(0,0),autonEC.get(0,0)};
@@ -96,7 +70,8 @@ public class Main {
 							   autonEC.get(i, 0), 
 							   climbEC.get(i, 0), 
 							   teleopEC.get(i, 0), 
-							   totalEC.get(i, 0)); 
+							   totalEC.get(i, 0));
+			
 			totalr[0] = Math.min(totalr[0],totalEC.get(i,0));
 			totalr[1] = Math.max(totalr[1],totalEC.get(i,0));
 			
@@ -113,44 +88,31 @@ public class Main {
 		String s = "<style>.results tr td {border-right:1px solid #222222; border-bottom:1px solid #222222;}  .results .heading td {border-color:#CCCCCC; border-bottom: 2px solid #BBBBBB;}   </style><div style='width:504px;border:1px solid #EEEEEE; padding:1px;'><table class=\"results\" cellspacing='0' style='font-family:Calibri;text-align:center;border:1px solid #CCCCCC;'><tbody>";
 		s = s + "<tr class=\"heading\" style='background:#EEEEEE;'><td style='width:50px; border-bottom:1px solid #CCCCCC;'>Team</td><td style='width:50px'>Standing</td><td style='width:100px;'>Autonomous</td><td style='width:100px;'>Climb</td><td style='width:100px;'>Teleop</td><td style='width:100px;'>Total</td></tr>";
 		
-		int color1 = 0x32C850;
-		int color2 = 0xC8F014;
-		int color3 = 0xC81428;
 		for (int i = 0; i < autonEC.getRowDimension(); i++)
 		{
 			s = s + "<tr>";
 			s = s + "<td style=\"border-right:2px solid #BBBBBB; border-bottom:1px solid #CCCCCC; background:"  + "#EEEEEE" + "\">" + (int)teamStandings.getBody()[1][i] + "</td>";
-			s = s + "<td style=\"background:" + getWarmCold(color1, color2, color3, 1, autonEC.getRowDimension()-(i+1), autonEC.getRowDimension())+ "\">" + (i+1) + "</td>";
-			s = s + "<td style=\"background:" + getWarmCold(color1, color2, color3, autonr[0], autonEC.get(i,0), autonr[1]) + "\">" + truncate(autonEC.get(i,0),2) + "</td>";
-			s = s + "<td style=\"background:" + getWarmCold(color1, color2, color3, climbr[0], climbEC.get(i,0), climbr[1]) + "\">" + truncate(climbEC.get(i,0),2) + "</td>";
-			s = s + "<td style=\"background:" + getWarmCold(color1, color2, color3, teleopr[0], teleopEC.get(i,0), teleopr[1]) + "\">" + truncate(teleopEC.get(i,0),2) + "</td>";
-			s = s + "<td style=\"background:" + getWarmCold(color1, color2, color3, totalr[0], totalEC.get(i,0), totalr[1]) + "\">" + truncate(totalEC.get(i,0),2) + "</td>";
+			s = s + "<td style=\"background:" + ColorUtils.colorToHexString(ColorUtils.getRedGreen(1, i+1, autonEC.getRowDimension(), true))+ "\">" + (i+1) + "</td>";
+			s = s + "<td style=\"background:" + ColorUtils.colorToHexString(ColorUtils.getRedGreen(autonr[0],autonEC.get(i,0),autonr[1])) + "\">" + truncate(autonEC.get(i,0),2) + "</td>";
+			s = s + "<td style=\"background:" + ColorUtils.colorToHexString(ColorUtils.getRedGreen(climbr[0],climbEC.get(i,0),climbr[1])) + "\">" + truncate(climbEC.get(i,0),2) + "</td>";
+			s = s + "<td style=\"background:" + ColorUtils.colorToHexString(ColorUtils.getRedGreen(teleopr[0],teleopEC.get(i,0),teleopr[1])) + "\">" + truncate(teleopEC.get(i,0),2) + "</td>";
+			s = s + "<td style=\"background:" + ColorUtils.colorToHexString(ColorUtils.getRedGreen(totalr[0],totalEC.get(i,0),totalr[1])) + "\">" + truncate(totalEC.get(i,0),2) + "</td>";
 			s = s + "</tr>";
 		}
 		s = s + "</tbody></table></div>";
-		System.out.println("\n\n\n\n\n" + s);
 		
-		/// PREDICT
-		
-		int[][] allianceMembers = {
-		/* 1 */	{33, 1718, 247},
-		/* 2 */	{2619, 3302, 245},
-		/* 3 */	{573, 3098, 1684},
-		/* 4 */	{2145, 2612, 3620},
-		/* 5 */	{51, 4810, 703},
-		/* 6 */	{4382, 4405, 1504},
-		/* 7 */	{3322, 1025, 548},
-		/* 8 */	{3570, 3535, 3667}
-		};
-		
-		double[] allianceValues = new double[allianceMembers.length];
-		for(int i = 0; i < allianceMembers.length; i++){
-			for(int j = 0; j < 3; j++){
-				allianceValues[i] += totalEC.get(teamHash.get(allianceMembers[i][j]) - 1, 0);
-			}
+		// Write to File
+		BufferedWriter br;
+		try {
+			br = new BufferedWriter(new FileWriter("data.html"));
+			br.write(s);
+			br.close();
+			java.awt.Desktop.getDesktop().browse(new URI("data.html"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
 		}
-		
-		System.out.println(Arrays.toString(allianceValues));
 	}
 
 	
