0a154f8f05e960e3ea72986a8fd3a71ae38c4fe1
blueshaman <blueshamenblutek@gmail.com>
2013-04-02 00:30:37 -0300

    Update main.js

--- web/main.js
+++ web/main.js
@@ -1,13 +1,15 @@
 
 
 
-
 //// JAVASCRIPT-Y STUFF BELOW -------------------------------------------------
 
 var pageMatches;
 var pageRankings;
 
-
+/**
+ * Repeatedly occurs until both pageMatches and pageRankings report that they have finished scraping and parsing.
+ * Then, inside the "if" , the remainder of processing may continue. Might want to move this inside of the FRC event?
+ */
 function waitForData() {
   if (pageMatches.ready && pageRankings.ready) {
 		document.getElementById("thedata").innerHTML = "<tr><td colspan=\"6\"><em>Data ready; computing.</em></td></tr>";
@@ -17,9 +19,9 @@ function waitForData() {
 }
 
 /**
- * Synch call so that the page doesn't freeze when it starts;
- * reduces page load time at the cost of causing a "waiting for data" message
- * to be displayed for a second.
+ * Initial table text is "waiting for scripts" so this modifies it to state "waiting for data".
+ * Create the Page objects necessary for the event (to be encapsulated by an FRCEvent object).
+ * Start waiting for the scrape and parse to finish at "waitForData()".
  */
 function Main() {
 	console.log("[Main] started");
