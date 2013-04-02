28b43d7770cd950df8f57d0de9686211bc372bd0
Curtis Fenner <cfenneremail@gmail.com>
2013-04-02 10:55:36 -0400

    FRCEvent made and integrated
    Some stuff to think about;
    I don't think FRCEvent should do any output at all if possible; only
    collect and process data (and have other things do the outputting).
    In that spirit, no longer is it writing out to the table the little
    messages. Instead we'll have Main.js do that.
    (This is good so that we can have multiple FRCEvents and not have them
    messing with one anotheR).
    .status represents the status message for that FRC Event.

---
+++ web/frcevent.js
@@ -0,0 +1,55 @@
+//// FRCEvent -----------------------------------------------------------------
+
+/**
+Represents all of the pages and data necessary for all of the calculations about an Event.
+
+baseurl: e.g., 2013comp/Events/MIGBL
+
+.pageRankings a Page for rankings.html
+.pageMatches a Page for matchresults.html
+
+.qualtable a Table for the qualification matches on matchresults.html (Will be undefined if the competition is not begun)
+.elimtable a Table for the elimination matches on matchresults.html (MAY BE UNDEFINED IF THEY HAVE NO YET BEGUN)
+.rankingstable a Table for the data on rankings.html
+
+**/
+function FRCEvent(baseurl,eventname) {
+	this.url = baseurl;
+	this.eventname = eventname;
+	this.status = "Waiting for data.";
+	this.pageRankings = new Page(baseurl + "rankings.html"); //Generate the table for Rankings
+	this.pageMatches = new Page(baseurl + "matchresults.html"); //Generate the table for the match results
+	var me = this;
+	this.waitForData = function() {
+		//Me is used instead of "this" because this function is called via setTimeout.
+		//That means "this" is the window.
+		//Apparently, "public" calls that I make to the object (me) get their "this" back though, so the rest uses normal "this".
+		if (me.pageMatches.ready && me.pageRankings.ready) {
+			me.status = "Data collected. Preparing for processing.";
+			me.prepare();
+		} else {
+			setTimeout(waitForData,100);
+		}
+	}
+	/**
+	Do surface level processing: getting all of the tables set up and things.
+	Then, move on to "process()".
+	**/
+	this.prepare = function() {
+		//Do stuff with this.pageRankings and this.pageMatches
+		this.qualtable = this.pageMatches.tables[0];
+		this.elimtable = this.pageMatches.tables[1];
+		this.rankingstable = this.pageRankings.tables[0];
+		this.process();
+	}
+	
+	/**
+	Do all computations here, including building matricies and things.
+	Output NOTHING. Just provide data outlets.
+	**/
+	this.process = function() {
+		this.status = "Processing data."
+		
+	}
+	setTimeout(this.waitForData,100);
+}
--- web/main.js
+++ web/main.js
@@ -6,25 +6,20 @@ var pageRankings;
  * Repeatedly occurs until both pageMatches and pageRankings report that they have finished scraping and parsing.
  * Then, inside the "if" , the remainder of processing may continue. Might want to move this inside of the FRC event?
  */
-function waitForData() {
-  if (pageMatches.ready && pageRankings.ready) {
-		document.getElementById("oprdata").innerHTML = "<tr><td colspan=\"6\"><em>Data ready; computing.</em></td></tr>";
-	} else {
-		setTimeout(waitForData,100);
-	}
-}
+
 
 /**
  * Initial table text is "waiting for scripts" so this modifies it to state "waiting for data".
  * Create the Page objects necessary for the event (to be encapsulated by an FRCEvent object).
  * Start waiting for the scrape and parse to finish at "waitForData()".
  */
+ var frcevent;
 function Main() {
 	
 	// Display Waiting Message
 	document.getElementById("oprdata").innerHTML = "<tr><td colspan=\"6\"><em>Waiting for data from www2.USFIRST.org...</em></td></tr>";
-	
-	pageRankings = new Page(rankingsPage); //Generate the table for Rankings
-	pageMatches = new Page(resultsPage); //Generate the table for the match results
-	waitForData(); //Go to "waitForData" while the pages are loading.
+	frcevent = new FRCEvent(eventURL,eventName);
+	//pageRankings = new Page(rankingsPage); //Generate the table for Rankings
+	//pageMatches = new Page(resultsPage); //Generate the table for the match results
+	//waitForData(); //Go to "waitForData" while the pages are loading.
 }
